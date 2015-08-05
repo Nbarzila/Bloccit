f60b7f2e6a31f87f8d5ae22ad81c4245a8a0dd92
Natalie <nbarzila@gmail.com>
2015-08-05 14:59:39 -0400

    CRUDS2 update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,9 +8,10 @@ class PostsController < ApplicationController
   end
 
   def new
-<<<<<<< Updated upstream
      @post = Post.new
-     def create
+  end
+
+  def create
    @post = Post.new(params.require(:post).permit(:title, :body))
    if @post.save
      flash[:notice] = "Post was saved."
@@ -19,10 +20,8 @@ class PostsController < ApplicationController
      flash[:error] = "There was an error saving the post. Please try again."
      render :new
    end
- end
 end
 
-  def edit
     def edit
      @post = Post.find(params[:id])
    end
@@ -37,33 +36,4 @@ end
        render :edit
      end
    end
-=======
-    @post = Post.new
-  end
-
-  def create
-    @post = Post.new(params.require(:post).permit(:title, :body))
-    if @post.save
-      flash[:notice] = "Post was saved."
-      redirect_to @post
-    else
-      flash[:error] = "There was an error saving the post. Please try again."
-      render :new
-    end
-  end
-
-  def edit
-    @post = Post.find(params[:id])
->>>>>>> Stashed changes
-  end
-
-  def update
-     @post = Post.find(params[:id])
-     if @post.update_attributes(params.require(:post).permit(:title, :body))
-       flash[:notice] = "Post was updated."
-       redirect_to @post
-     else
-       flash[:error] = "There was an error saving the post. Please try again."
-       render :edit
-     end
-   end
+end
