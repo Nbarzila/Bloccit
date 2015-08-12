efa1266b7f7b3130c3c2c9819b376229026da80e
Natalie <nbarzila@gmail.com>
2015-08-11 22:20:29 -0400

    updates merge issues

--- app/models/post.rb
+++ app/models/post.rb
@@ -13,5 +13,7 @@ class Post < ActiveRecord::Base
   has_many :comments
   belongs_to :user
   default_scope { order('created_at DESC') }
+  scope :ordered_by_title, -> { reorder('title ASC')}
+  scope :ordered_by_reverse_created_at, -> { reorder('created_at DESC')}
 
 end
