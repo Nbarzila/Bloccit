64fd782c2b87ee65e17aa6037f1830769173f4ad
Natalie <nbarzila@gmail.com>
2015-08-27 17:28:34 -0400

    checkpooint43

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,11 @@ module ApplicationHelper
     "Natalie Barzilai"
   end
 
+    def form_group_tag(errors, &block)
+   if errors.any?
+     content_tag :div, capture(&block), class: 'form-group has-error'
+   else
+     content_tag :div, capture(&block), class: 'form-group'
+   end
+  end
 end
