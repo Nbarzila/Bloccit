923551f1465ef1334b15f14b3cc821223bda2c6f
Natalie <nbarzila@gmail.com>
2015-09-06 13:23:04 -0400

    images uploader

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,7 @@
 #  id         :integer          not null, primary key
 #  title      :string
 #  body       :text
+#  image      :references
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
@@ -21,4 +22,18 @@ class Post < ActiveRecord::Base
  validates :body, length: { minimum: 20 }, presence: true
  validates :topic, presence: true
  validates :user, presence: true
+end
+
+ def markdown_to_html(markdown)
+
+
+   <%= post.markdown_title %>
+   <%= post.markdown_body %>
+
+   private
+   renderer = Redcarpet::Render::HTML.new
+   extensions = {fenced_code_blocks: true}
+   redcarpet = Redcarpet::Markdown.new(renderer, extensions)
+   (redcarpet.render markdown).html_safe
+ end
 end
