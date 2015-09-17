7bae08eabc50173cfc90b1e37f2e46f618e8cf01
Natalie <nbarzila@gmail.com>
2015-09-17 16:46:10 -0400

    image upload fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :posts
-   mount_uploader :avatar, AvatarUploader
+   mount_uploader :image, ImageUploaderUploader
 
   def admin?
    role == 'admin'
