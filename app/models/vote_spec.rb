8e16029b90d2ffd031c101592d3340774af380a1
Natalie <nbarzila@gmail.com>
2015-10-05 17:34:56 -0400

    Complete intro to rspec assignment1

--- app/models/vote_spec.rb
+++ app/models/vote_spec.rb
@@ -1,26 +1,34 @@
-require 'rails_helper'
+  require 'rails_helper'
 
-describe Post do
+  describe Post do
   describe "vote methods" do
 
     before do
       @post = Post.create(title: 'post title', body: 'post body')
       3.times { @post.votes.create(value: 1)}
       2.times { @post.votes.create(value:-1)}
-      1.times { @post.votes.create(value: 0)}
+      2.times { @post.votes.create(value: 0)}
 
     end
 
 
 
-describe Vote do
+  describe Vote do
 
   describe "validations" do
     describe "value validation" do
       it "only allows -1 or 1 as values" do
         expect(@votes.value).to eq(1)
-        expect(@votes.value).to eq(-1)
+          result = vote.save
+      end
 
+      it "only allows -1 or 1 as values" do
+        expect(@votes.value).to eq(-1)
+          result = vote.save
+      end
+
+      it "does not allow other value" do
+        expect(@votes.value).to be_false
       end
     end
   end
