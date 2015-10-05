918ed1f5be221528e9deafe19bbf82d05afd14cf
Natalie <nbarzila@gmail.com>
2015-10-05 14:56:32 -0400

    assignment update

--- app/models/post.rb
+++ app/models/post.rb
@@ -23,17 +23,18 @@ class Post < ActiveRecord::Base
  validates :body, length: { minimum: 20 }, presence: true
  validates :topic, presence: true
  validates :user, presence: true
-end
+ end
 
  def markdown_to_html(markdown)
 
    <%= post.markdown_title %>
    <%= post.markdown_body %>
 
+ end
+end
    private
    renderer = Redcarpet::Render::HTML.new
    extensions = {fenced_code_blocks: true}
    redcarpet = Redcarpet::Markdown.new(renderer, extensions)
    (redcarpet.render markdown).html_safe
  end
-end
