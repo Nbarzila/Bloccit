e8bbb2c55bfb4b9f35259cfa4b687555069fe295
Natalie <nbarzila@gmail.com>
2015-10-05 15:13:34 -0400

    up1

--- app/models/post.rb
+++ app/models/post.rb
@@ -35,9 +35,11 @@ class Post < ActiveRecord::Base
    end
 
    private
-   renderer = Redcarpet::Render::HTML.new
-   extensions = {fenced_code_blocks: true}
-   redcarpet = Redcarpet::Markdown.new(renderer, extensions)
-   (redcarpet.render markdown).html_safe
+
+   def markdown_to_html(markdown)
+     renderer = Redcarpet::Render::HTML.new
+     extensions = {fenced_code_blocks: true}
+     redcarpet = Redcarpet::Markdown.new(renderer, extensions)
+     (redcarpet.render markdown).html_safe
+   end
  end
-end 
