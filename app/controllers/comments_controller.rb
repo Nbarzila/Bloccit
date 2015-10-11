e4f865d2c7c47f751a370216c107131f96566101
Natalie <nbarzila@gmail.com>
2015-10-11 16:45:25 -0400

    updates destroy

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,27 +1,28 @@
 class CommentsController < ApplicationController
 
   def create
-    @comment = Comment.find(params[:comment_id])
-    @comment = @post.comments.new(comment_params)
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.build(comment_params)
     @comment.user_id = current_user.id
+    @comment.post = @post
     if @comment.save
-      redirect_to [@post, @comment], notice: "Comment was saved successfully."
+      redirect_to [ @post.topic, @post], notice: "Comment was saved successfully."
     else
       flash[:error] = "Error creating comment. Please try again."
       render :new
     end
   end
-    def destroy
+
+  def destroy
    @comment = @post.comments.find(params[:id])
 
-   authorize @comment
-   if @comment.destroy
+    if @comment.destroy
      flash[:notice] = "Comment was removed."
-     redirect_to [@post]
-   else
+     redirect_to [@post.topic, @post]
+    else
      flash[:error] = "Comment could not be deleted. Try again."
-     redirect_to [@post]
-   end
+     redirect_to [@post.topic, @post]
+    end
   end
 
 
