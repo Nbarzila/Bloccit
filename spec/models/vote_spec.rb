aa8382062c047157c86a42accc004b388fc08fdb
Natalie <nbarzila@gmail.com>
2015-10-14 16:39:21 -0400

    vote up 2 trying to fix

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -7,7 +7,7 @@ describe Vote do
 
         vote_1 = Vote.new(value: 1)
         vote_2 = Vote.new(value: -1)
-        vote_error = Vote.new(value: 2)
+        vote_error = Vote.new(value: 0)
 
         expect(vote_1.valid?).to eq(true)
         expect(vote_2.valid?).to eq(true)
