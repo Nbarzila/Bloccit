7c1b96bc6fffeaf79ac82ddcfd2969b433740e29
Natalie <nbarzila@gmail.com>
2015-10-14 16:09:43 -0400

    vote spec

---
+++ spec/models/vote_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe Vote do
+  describe "validations" do
+    describe "value validation" do
+      it "only allows -1 or 1 as values" do
+
+        vote_1 = Vote.new(value: 1)
+        expect(vote_1.valid?).to eq(true)
+
+        vote_2 = Vote.new(value: -1)
+        expect(vote_2.valid?).to eq(true)
+
+        vote_error = Vote.new(value: 2)
+        expect(vote_error.valid?).to eq(false)
+
+      end
+    end
+  end
+end
