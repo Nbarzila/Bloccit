f1bfe739de49bf1e815a21bcbb9b5fb271db0aff
Natalie <nbarzila@gmail.com>
2015-10-13 23:49:48 -0400

    master vote update

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,20 +10,22 @@
 #  updated_at :datetime         not null
 #
 
-class Post < ActiveRecord::Base
 
-  has_many :comments, dependent: :destroy
-  has_many :votes, dependent: :destroy
 
-  mount_uploader :image, ImageUploaderUploader
-  mount_uploader :avatar, AvatarUploader
-  belongs_to :user
-  belongs_to :topic
-  default_scope { order('rank DESC') }
-  scope :ordered_by_title, -> { reorder('title ASC')}
-  #scope :ordered_by_reverse_created_at, -> { reorder('created_at DESC')}
+  class Post < ActiveRecord::Base
+
+    has_many :comments, dependent: :destroy
+    has_many :votes, dependent: :destroy
+    mount_uploader :avatar, AvatarUploader
+    belongs_to :user
+    belongs_to :topic
+    default_scope { order('rank DESC') }
+    scope :ordered_by_title, -> { reorder('title ASC')}
+    scope :ordered_by_reverse_created_at, -> { reorder('created_at DESC')}
+
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
+
   after_create :create_vote
   # validates :topic, presence: true
   # validates :user, presence: true
