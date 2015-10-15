d60710d03b609162907c982ceacfab55f19de350
Natalie <nbarzila@gmail.com>
2015-10-14 21:56:22 -0400

    checkpoint55 start

---
+++ spec/controllers/comments_controller.rb
@@ -0,0 +1,41 @@
+class CommentsController < ApplicationController
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.new(comment_params)
+    @comment.user_id = current_user.id
+      authorize @comment
+    if @comment.save
+      redirect_to [@post.topic, @post], notice: "Comment was saved successfully."
+    else
+      redirect_to [@post, @comment], notice: "Error creating comment. Please try again."
+    end
+  end
+
+
+  def new
+     @post = Post.find(params[:post_id])
+     @comment = Comment.new
+        authorize @comment
+  end
+
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+      authorize @comment
+   if @comment.destroy
+     flash[:notice] = "Comment was removed."
+     redirect_to [@post.topic, @post]
+   else
+     flash[:error] = "Comment could not be deleted. Try again."
+     redirect_to [@post, @comment]
+   end
+ end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+end
