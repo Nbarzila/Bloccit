c560b2b745109d23c13b817511e095a37901f07a
Natalie <nbarzila@gmail.com>
2015-10-28 19:48:28 -0400

    publicprofiles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-
-  params.require(:user).permit(:name, :avatar, :email_favorites)
+    params.require(:user).permit(:name, :avatar, :email_favorites)
+  end
 end
