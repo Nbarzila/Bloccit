039efe129de527824a26a6ec007ec661b11e7c7f
Natalie <nbarzila@gmail.com>
2015-10-28 15:31:26 -0400

    privatetopicsassignments

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -11,8 +11,11 @@
 #
 
 class Topic < ActiveRecord::Base
-    has_many :posts, dependent: :destroy
+    has_many  :posts, dependent: :destroy
     validates :name, length: { minimum: 5 }, presence: true
+    validates :description, presence: true
 
-    scope :visible_to, -> (user) { user ? all : where(public: true) }
+    scope :publicly_viewable, -> { where(public: true) }
+    scope :privately_viewable, -> { where(public: false) }
+    scope :visible_to, -> (user) {user ? all : publicly_viewable}
 end
